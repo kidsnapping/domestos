8cf55d6284b568f16e016c711bf9443c3dac5472
anon <makarmikryukov@gmail.com>
2026-03-21 19:26:07 +0300

    tests of JavaParser futures

--- src/main/java/core/Test.java
+++ src/main/java/core/Test.java
@@ -1,7 +1,19 @@
 package core;
 
-public class Test {
-    public static void main(String[] args) {
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.printer.YamlPrinter;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Test {
+    private static final String FILE_PATH =
+            "src/main/java/core/AnalyzerApplication.java";
+    public static void main(String[] args) throws Exception {
+        CompilationUnit cu = StaticJavaParser
+                .parse(Files.newInputStream(Paths.get(FILE_PATH)));
+        YamlPrinter printer = new YamlPrinter(true);
+        System.out.println(printer.output(cu));
     }
 }
