91d3b1ab3f3b813afb9094fec73b6da2e11fda01
anon <makarmikryukov@gmail.com>
2026-03-21 14:07:11 +0300

    Added new dir Model and ProjectParser.java class

--- src/main/java/parser/ProjectParser.java
+++ src/main/java/parser/ProjectParser.java
@@ -1,5 +1,57 @@
 package parser;
 
-public class ProjectParser {
+import model.ClassModel;
+import model.ProjectModel;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ProjectParser {
+    private final Path projectRoot;
+
+    public ProjectParser(Path projectRoot) {
+        this.projectRoot = projectRoot;
+    }
+
+    public ProjectModel parseProject() {
+        ProjectModel projectModel = new ProjectModel();
+
+        List<Path> javaFiles = findJavaFiles();
+
+        for (Path file : javaFiles) {
+            try {
+                FileParser fileParser = new FileParser(file);
+                ClassModel classModel = fileParser.parse();
+
+                if (classModel != null) {
+                    projectModel.addClass(classModel);
+                }
+
+            } catch (Exception e) {
+                System.err.println("Ошибка при обработке файла: " + file);
+                e.printStackTrace();
+            }
+        }
+
+        return projectModel;
+    }
+
+    private List<Path> findJavaFiles() {
+        List<Path> javaFiles = new ArrayList<>();
+
+        try (Stream<Path> paths = Files.walk(projectRoot)) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".java"))
+                    .forEach(javaFiles::add);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка при сканировании проекта", e);
+        }
+
+        return javaFiles;
+    }
 }
