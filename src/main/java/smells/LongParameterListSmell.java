be3501d5a47b87d2d7659403fa4040cc7602276d
anonHome <makarmikryukov@gmail.com>
2026-05-15 19:31:59 +0000

    changed count of max_parameters

--- src/main/java/smells/LongParameterListSmell.java
+++ src/main/java/smells/LongParameterListSmell.java
@@ -4,7 +4,7 @@ import model.MethodModel;
 
 public class LongParameterListSmell {
 
-    private static final int MAX_PARAMETERS = 4;
+    private static final int MAX_PARAMETERS = 5;
 
     public boolean hasLongParameterList(MethodModel method) {
         return method.getParameterCount() > MAX_PARAMETERS;
